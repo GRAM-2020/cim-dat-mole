ca60580e63ce5cb0f59827264b030052ccfb0714
Alexander Ruppert <aruppert@pm.me>
2020-03-30 10:16:44 +0200

    Add basic layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+(function() {
+  let fieldSize;
+  const b1 = select("#b1");
+  const game = select("#game");
 
+  // initial Values
+  function initVars() {
+    startingLifes = 3;
+    fieldSize = 9;
+    // imgMix = shuffle1(loadImgSetting());
+  }
+
+  function initField() {
+    let img;
+    for (let i = 0; i < fieldSize; i++) {
+      img = create("img");
+      img.src = "img/hole.gif";
+      img.setAttribute("data-nr", i);
+      // img.addEventListener("click", checkImg); //
+      game.appendChild(img);
+    }
+  }
+
+  // Ausführung
+  initVars();
+  initField();
+})();
