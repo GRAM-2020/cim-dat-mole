eec7c2394848272f67a497d60c095e2c8d4dcdab
Alexander Ruppert <aruppert@pm.me>
2020-04-01 09:12:25 +0200

    Add auto death functionality (#9)
    * Add life counting
    
    * Added Comments, Added an hit Variable and put back the eventhandler into the initField and crasyMile. We can't put them inside the startGame cause it will always add an eventlistener to the img when we start a new Instance of startGame (jiks)
    
    * Added the mole got hit png and reset the png at the start of an Instance of a Game
    
    * Moved hit = false inside the init Vars
    
    * Added one Turn, that will end if life is 0
    
    Co-authored-by: Marco Grahl <manograhl@gmail.com>

--- js/lib.js
+++ js/lib.js
@@ -2,3 +2,9 @@ const select = selector => document.querySelector(selector);
 const selectAll = selector => Array.from(document.querySelectorAll(selector));
 const create = el => document.createElement(el);
 
+// just a littel helperfkt to toggle the attribute active true || false
+function toggle(arr) {
+	let toggle = (arr.getAttribute('data-active') === 'true');
+	toggle = !toggle
+	arr.dataset.active = toggle
+}
--- js/main.js
+++ js/main.js
@@ -1,88 +1,149 @@
 (function () {
-  let fieldSize, startingLifes;
+  //Variables we will give an initial Value
+  let fieldSize, startingLifes, peekTime, turnTime, lifeCounter, hit, runningGame, fieldActive;
+
+  //Variables that have a connection to the Dom Element
   const startButton = select("#start");
-  startButton.addEventListener('click', countdown)
   let countdownInput = select("#countdown")
   const hole = select("#hole");
+  let life = select("#life")
   const mole = select("#mole");
-  console.log(countdownInput.value)
-  // inital Values
+
+  // inital Values we set here
   function initVars() {
     countdownInput.value = "Ready?";
+    runningGame = false;
     startingLifes = 3;
+    life.value = startingLifes;
     fieldSize = 9;
+    peekTime = 1000;
+    turnTime = 1200;
+    fieldActive = false;
+    hit = false;
   }
 
-  function countdown() {
-    setTimeout(() => countdownInput.value = 3, 1000)
-    setTimeout(() => countdownInput.value = 2, 2000)
-    setTimeout(() => countdownInput.value = 1, 3000)
-    setTimeout(() => { countdownInput.value = "GO!"; startGame() }, 4000)
-
-  }
-
+  // ****************************  The Board  ****************************
+  // Here we laod in the holefield, which gets the attribute active=true,
+  // that means it has an higher i-index than active=false and is visible
   function initField() {
     let img;
     for (let i = 0; i < fieldSize; i++) {
       img = create("img");
-      img.src = "img/hole.gif";
+      img.draggable = false
+      img.src = "img/hole.png";
       img.setAttribute("data-active", 'true');
       img.setAttribute("data-img", "hole");
+      img.addEventListener('click', checkClick)
       hole.appendChild(img);
-      img.addEventListener('click', checkActive)
     }
   }
+
+  // Here we laod in the molefield, with active false, both hole and mole get img attribute
+  // to let us know if we clickt a hole or mole
   function crazyMole() {
     let img;
     for (let i = 0; i < fieldSize; i++) {
       img = create("img");
-      img.src = "img/mole.gif";
+      img.draggable = false
+      img.src = "img/mole.png";
       img.setAttribute("data-active", 'false');
       img.setAttribute("data-img", "mole");
+      img.addEventListener('click', checkClick)
       mole.appendChild(img);
-      img.addEventListener('click', checkActive)
     }
   }
 
+  // ****************************  Starting the game + Countdown ****************************
+  // Countdown for the game u can only click once, the variable runningGame is set to true
+  startButton.addEventListener('click', countdown)
+  function countdown() {
+    if (!runningGame) {
+      runningGame = true
+      setTimeout(() => countdownInput.value = 3, 1000)
+      setTimeout(() => countdownInput.value = 2, 2000)
+      setTimeout(() => countdownInput.value = 1, 3000)
+      setTimeout(() => { countdownInput.value = "GO!"; startGame() }, 4000)
+    }
 
+  }
 
+  // **************************** Check for Click Event  ****************************
+  //Here we check for an click Event if its a hole u lose 1 life, u always get a hit, prevents loosing 2 lifes
+  function checkClick(e) {
+    if (fieldActive) {
+      if (this.getAttribute('data-active') === "true" && this.getAttribute('data-img') === 'hole') {
+        hit = true
+        life.value--
+      } else {
+        hit = true
+        //if u hit a mole u get the mole_hited img, gets reseted after each instance
+        this.src = 'img/mole_hited.png'
+
+      }
+      // Here we can check if you hit a hole and have 0 Lifes the game is over
+      if (life.value === "0") {
+        uLost()
+      }
+    }
+  }
+
+  // **************************** Logic for one Instance of the Game  ****************************
   function startGame() {
-    const moleImgs = selectAll("[data-img='mole']");
+
     const holeImgs = selectAll("[data-img='hole']");
-    const random = Math.floor(Math.random() * fieldSize)
+    const moleImgs = selectAll("[data-img='mole']");
+    const allImgs = selectAll('img')
 
-    function toggle(arr) {
-      let toggle = (arr.getAttribute('data-active') === 'true');
-      toggle = !toggle
+    //if a mole_hited png is still in the field just overwrite it at the start of an instance
 
-      arr.dataset.active = toggle
-    }
+    fieldActive = true // now the function checkClick will update the score
 
-    toggle(moleImgs[2]);
-    setTimeout(() => toggle(moleImgs[2]), 3000)
 
+
+    function oneTurn() {
+      const random = Math.floor(Math.random() * fieldSize);
+      moleImgs.filter(i => (i.src !== 'img/mole.png') ? i.src = 'img/mole.png' : i)
+      // Here we say if u didn't hit anything u will loose a life, and if u did hit something we reset the Value hit to false
+      function oneTurnCheck() {
+        toggle(moleImgs[random]);
+        // hit is set to false in the init Vars
+        if (!hit) {
+          life.value--;
+          if (life.value === "0") {
+            uLost()
+          }
+        }
+        hit = false;
+      }
+      // here we start one turn and test if hit is true or false, than we restart a new Instance of the Game
+      toggle(moleImgs[random]);
+      setTimeout(() => oneTurnCheck(), peekTime)
+
+      setTimeout(() => {
+        if (life.value > 0) {
+          oneTurn()
+        }
+      }
+
+        , peekTime + turnTime)
+    };
+    oneTurn();
   }
 
 
-  function checkActive(e) {
-    console.log(this.getAttribute('data-active'))
-    console.log(e);
-    console.log(this)
-    if (this.getAttribute('data-active') === "true" && this.getAttribute('data-img') === 'hole') {
-      console.log('u lost a life')
-    } else {
-      console.log('inactive')
-    }
-    // console.log(e.dataset.active)
-    // if (e.dataset.active('data-active') === 1) {
-    //   console.log('active')
-    // }
+
+
+  // **************************** EndDisplay  ****************************
+  // needs to live outside so the startGame and the checkClick can use the fkt
+  function uLost() {
+    alert('u lost')
   }
+
+
 
   // Ausführung
   initVars();
   initField();
   crazyMole();
-
 })();
 
